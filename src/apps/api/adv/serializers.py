a745c6f84afcd34db10daaca646f2ebd6b711291
polecie <68993459+polecie@users.noreply.github.com>
2024-08-14 14:56:57 +0400

    Update serializers.py

--- src/apps/api/adv/serializers.py
+++ src/apps/api/adv/serializers.py
@@ -4,6 +4,9 @@ from apps.adv.models import Advert
 
 
 class AdvertSerializer(serializers.ModelSerializer):
+    city = serializers.CharField(source="city.name")
+    category = serializers.CharField(source="category.name")
+    
     class Meta:
         model = Advert
         fields = "__all__"
